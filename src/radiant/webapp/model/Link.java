7ce1ae821d2153d2931e2aaf3458f23b0b2b4b23
IvanRadiantStd <iv.radiant@yandex.ru>
2017-04-20 15:56:10 +0300

    Class 'link' added to the project

---
+++ src/radiant/webapp/model/Link.java
@@ -0,0 +1,14 @@
+package radiant.webapp.model;
+
+
+public class Link {
+
+    private final String name;
+    private final String url;
+
+    public Link(String name, String url){
+        this.name = name;
+        this.url = url;
+    }
+
+}
