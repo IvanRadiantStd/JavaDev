135842726886078a07250bf5fa7d7dd471cbe986
IvanRadiantStd <iv.radiant@yandex.ru>
2017-04-21 19:23:34 +0300

    Enhance Contact Type

--- src/radiant/webapp/model/ContactType.java
+++ src/radiant/webapp/model/ContactType.java
@@ -1,11 +1,21 @@
 package radiant.webapp.model;
 
 
-public enum  ContactType {
-    PHONE,
-    MOBILE,
-    HOME_PHONE,
-    SKYPE,
-    MAIL,
-    ICQ
+public enum ContactType {
+    PHONE("Тел."),
+    MOBILE("Мобильный"),
+    HOME_PHONE("Домашний тел."),
+    SKYPE("Skype"),
+    MAIL("Почта"),
+    ICQ("ICQ");
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    ContactType(String title){
+        this.title = title;
+    }
 }
