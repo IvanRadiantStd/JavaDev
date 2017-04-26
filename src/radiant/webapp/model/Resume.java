eb12ba92453e4df6472a20f95c0536d87734dacc
IvanRadiantStd <iv.radiant@yandex.ru>
2017-04-26 16:03:38 +0300

    Enhance Contact Type

--- src/radiant/webapp/model/Resume.java
+++ src/radiant/webapp/model/Resume.java
@@ -5,10 +5,19 @@ import java.util.List;
 
 public class Resume {
 
+    private String uuid;
     private String fullName;
     private String location;
     private String homePage;
     private List<Contact> contacts;
     private List<Section> sections;
 
+    public Resume(String uuid, String fullName, String location, String homePage, List<Contact> contacts, List<Section> sections) {
+        this.uuid = uuid;
+        this.fullName = fullName;
+        this.location = location;
+        this.homePage = homePage;
+        this.contacts = contacts;
+        this.sections = sections;
+    }
 }
---
+++ src/radiant/webapp/storage/ArrayStorage.java
@@ -0,0 +1,47 @@
+package radiant.webapp.storage;
+
+
+import radiant.webapp.model.Resume;
+
+import java.util.Collection;
+
+public class ArrayStorage implements IStorage {
+
+
+    private Resume[] array;
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void save(Resume r) {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public Resume load(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Collection<Resume> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
